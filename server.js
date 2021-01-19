7b822550ff2bf8e8002f295a10de39703a35e098
Taylor Niles <tniles320@gmail.com>
2021-01-19 14:52:11 -0800

    working on deploying to heroku

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
