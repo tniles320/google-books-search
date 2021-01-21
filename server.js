fa5f3a4e9e62322a89e741aee8fd929651abd392
Taylor Niles <tniles320@gmail.com>
2021-01-20 16:53:53 -0800

    updating to attempt to force https

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
