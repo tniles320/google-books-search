ab1df27ad1b3408b0c6a53e52ca5b70adb56b2f1
Taylor Niles <tniles320@gmail.com>
2021-01-20 16:48:26 -0800

    updating to attempt to force https

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
